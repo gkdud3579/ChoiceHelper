dfb3b75893f462e31ad406b106785a84dcdaa272
Hayoung Shim <gkdud3579@gmail.com>
2024-04-03 17:10:32 +0900

    Feat: InputPage 기능구현
    입력값 동적으로 처리 구현

--- choicePicker/src/Pages/Input-page/InputPage.tsx
+++ choicePicker/src/Pages/Input-page/InputPage.tsx
@@ -1,15 +1,36 @@
+import React, { useState } from 'react';
 import './InputPage.scss';
 
 function InputPage() {
+  // 입력 값을 관리할 상태 초기화
+  const [choices, setChoices] = useState(["", "", "", "", ""]);
+
+  // 입력 값 변경 핸들러
+  const handleChange = (index, value) => {
+    const newChoices = [...choices];
+    newChoices[index] = value;
+    setChoices(newChoices);
+  };
+
+  // 제출 버튼 클릭 핸들러
+  const handleSubmit = () => {
+    console.log(choices);
+  };
+
   return (
     <div className="input-page">
-      <div className="input-title">enter your 5 choices</div>
-      <input className="input" type="text" value="choice 1" />
-      <input className="input" type="text" value="choice 2" />
-      <input className="input" type="text" value="choice 3" />
-      <input className="input" type="text" value="choice 4" />
-      <input className="input" type="text" value="choice 5" />
-      <button className="submit-button">Submit</button>
+      <div className="input-title">Enter your 5 choices</div>
+      {choices.map((choice, index) => (
+        <input
+          key={index}
+          className="input"
+          type="text"
+          value={choice}
+          onChange={(e) => handleChange(index, e.target.value)}
+          placeholder={`Choice ${index + 1}`}
+        />
+      ))}
+      <button className="submit-button" onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
