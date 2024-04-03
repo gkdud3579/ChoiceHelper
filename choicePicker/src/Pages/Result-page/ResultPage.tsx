2078898800e75a7054882fed10153abfcd4e528a
Hayoung Shim <gkdud3579@gmail.com>
2024-04-03 21:48:03 +0900

    Design: 언어별 결과값 폰트 적용
    한국어:잘난체/영어:Press Start 2P

--- choicePicker/src/Pages/Result-page/ResultPage.tsx
+++ choicePicker/src/Pages/Result-page/ResultPage.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "./ResultPage.scss";
 
 function ResultPage() {
   const location = useLocation();
+  const navigate = useNavigate();
   const { chosen } = location.state || {};
 
-  const navigate = useNavigate();
+  const determineFont = (value) => {
+    const koreanRegex = /[가-힣]/;
+    return koreanRegex.test(value) ? "korean-font" : "english-font";
+  };
 
   const handleClick = () => {
     navigate("/");
   };
 
+  const resultClass = chosen ? determineFont(chosen) : "";
+
   return (
     <div className="result-page">
       <div className="result-title">This is your choice!</div>
-      <div className="result">{chosen || "No choice selected"}</div>
+      <div className={`result ${resultClass}`}>
+        {chosen || "No choice selected"}
+      </div>
       <a>share your choice</a>
       <button onClick={handleClick} className="retry-button">
         retry
