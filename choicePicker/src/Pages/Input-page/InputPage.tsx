46b1cad75e563fb2ea36e2c1a9b3978f7ea9cf8b
Hayoung Shim <gkdud3579@gmail.com>
2024-04-05 12:43:50 +0900

    Fix: 빈 배열값 걸러내기

--- choicePicker/src/Pages/Input-page/InputPage.tsx
+++ choicePicker/src/Pages/Input-page/InputPage.tsx
@@ -22,8 +22,8 @@ function InputPage() {
 
   // 제출 버튼 클릭 핸들러
   const handleSubmit = () => {
-    // const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    navigate('/loading', { state: { choices: choices } });
+    const filteredChoices = choices.filter((choice) => choice.trim() !== "");
+    navigate("/loading", { state: { choices: filteredChoices } });
   };
 
   return (
